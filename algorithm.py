4f054e9cd723ff5d49680b562db17f9ec689e3cc
Lukas Martinelli <me@lukasmartinelli.ch>
2014-11-03 22:43:45 +0100

    Added source code

---
+++ algorithm.py
@@ -0,0 +1,69 @@
+from collections import Counter
+from pathlib import Path
+
+
+ALGORITHMS = [# Non-modifying sequence operations
+              "all_of", "any_of", "none_of", "for_each", "count", "count_if",
+              "mismatch", "equal", "find", "find_if", "find_if_not", "find_end",
+              "find_first_of", "adjacent_find", "search", "search_n",
+              # Modifying sequence operations
+              "copy", "copy_if", "copy_n", "copy_backward", "move", "move_backward",
+              "fill", "fill_n", "transform", "generate", "generate_n", "remove",
+              "remove_if", "remove_copy", "remove_copy_if", "replace", "replace_if",
+              "replace_copy", "replace_copy_if", "swap", "swap_ranges", "iter_swap",
+              "reverse", "reverse_copy", "rotate", "rotate_copy", "random_shuffle",
+              "shuffle", "unique", "unique_copy"
+              #  Partitioning operations
+              "is_partitioned", "partition", "partition_copy", "stable_partition",
+              "partition_point",
+              # Sorting operations
+              "is_sorted", "is_sorted_until", "sort", "partial_sort",
+              "partial_sort_copy", "stable_sort", "nth_element",
+              # Binary search operations (on sorted ranges)
+              "lower_bound", "upper_bound", "binary_search", "equal_range",
+              # Set operations (on sorted ranges)
+              "merge", "inplace_merge", "includes", "set_difference",
+              "set_intersection", "set_symmetric_difference", "set_union",
+              # Heap operations
+              "is_heap", "is_heap_until", "make_heap", "push_heap",
+              "pop_heap", "sort_heap",
+              # Minimum/maximum operations
+              "max", "max_element", "min", "min_element", "minmax",
+              "minmax_element", "lexicographical_compare", "is_permutation",
+              "next_permutation", "prev_permutation",
+              # Numeric operations
+              "iota", "accumulate", "inner_product", "adjacent_difference",
+              "partial_sum"]
+
+
+def _has_headers(cpp_file):
+    for line in cpp_file:
+        if "<algorithm>" in line or "<numeric>" in line:
+            return True
+    return False
+
+
+def count_algorithms(cpp_file):
+    """Count all algorithms in a file"""
+    algorithms = Counter({key: 0 for key in ALGORITHMS})
+    try:
+        for line in cpp_file:
+            for algo in algorithms:
+                if algo in line:
+                    algorithms[algo] += 1
+    except UnicodeDecodeError:
+        # We cannot do anything if we don't know the encoding of the file
+        pass
+    return algorithms
+
+
+def filter_cpp_files(repo):
+    """Filter out all cpp files that include an <algorithm> or <numeric> header"""
+    repo_path = Path(repo.dir)
+    try:
+        for cpp_path in repo_path.glob("**/*.cpp"):
+            with cpp_path.open() as cpp_file:
+                if _has_headers(cpp_file):
+                    yield cpp_file
+    except OSError:
+        print("Too many symlinks in repository {1}".format(repo_path))
---
+++ algostat.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+"""
+Find the most frequently used algorithms of the C++ standard library for
+C++ projects on Github.
+
+Usage:
+    algostat.py -u USERNAME -p=PASSWORD
+    algostat.py (-h | --help)
+
+Options:
+    -h --help         Show this screen
+    -u USERNAME       Github Account to make API requests with
+    -p PASSWORD       Password or OAuth token of your Github Account
+"""
+import os
+import tempfile
+import shutil
+import subprocess
+from pathlib import Path
+from collections import Counter
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+from docopt import docopt
+
+from algorithm import filter_cpp_files, count_algorithms
+
+
+class TemporaryGitRepo:
+    def __init__(self, git_url):
+        self.git_url = git_url
+
+    def __enter__(self):
+        self.dir = tempfile.mkdtemp(suffix="cpp")
+        FNULL = open(os.devnull, 'w')
+        print("Cloning {0} into {1}".format(git_url, self.dir))
+        subprocess.check_call(["git", "clone", self.git_url, self.dir],
+                               stdout=FNULL, stderr=subprocess.STDOUT)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        shutil.rmtree(self.dir)
+
+
+def get_cpp_repositories(make_request):
+    """Search for the top 1000 cpp repos in github and return their git url"""
+    url = "https://api.github.com/search/repositories?q=language:cpp&per_page=100"
+    response = make_request(url)
+
+    while "next" in response.links.keys():
+        url = response.links["next"]["url"]
+        response = make_request(url)
+        for repo in response.json()["items"]:
+            yield repo["git_url"]
+
+
+def count_repo_algorithms(repo):
+    """Count all algorithms in a repository"""
+    repo_algorithms = Counter()
+    for cpp_path in filter_cpp_files(repo):
+        repo_algorithms += count_algorithms(cpp_path)
+    print("Found algorithms for {0}".format(repo.dir))
+    print(repo_algorithms)
+    return repo_algorithms
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    def make_request(url):
+        headers = {"Accept": "application/vnd.github.v3+json"}
+        auth=HTTPBasicAuth(args["-u"], args["-p"])
+        return requests.get(url, headers=headers, auth=auth)
+
+    all_algorithms = Counter()
+    for git_url in get_cpp_repositories(make_request):
+        with TemporaryGitRepo(git_url) as repo:
+            all_algorithms += count_repo_algorithms(repo)
+
+    print("-----------RESULT------------")
+    print(all_algorithms)
